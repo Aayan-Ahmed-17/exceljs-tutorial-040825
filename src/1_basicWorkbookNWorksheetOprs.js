999ce1fb7c39e7d7275908d84d0b235dc925749d
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-04 16:01:41 +0500

    2 methods approach tried for time manipulation

--- src/1_basicWorkbookNWorksheetOprs.js
+++ src/1_basicWorkbookNWorksheetOprs.js
@@ -1,13 +1,21 @@
 import Exceljs from "exceljs";
 
 export function basicWorkbookNWorksheetOprs() {
-  const workbook = new Exceljs.Workbook();
-  let { creator, created, lastModifiedBy } = workbook;
+  const workbook = new Exceljs.Workbook(); //workbook created
+  workbook.properties.date1904 = true;
+  let { creator, created, lastModifiedBy } = workbook; //mostly used props
 
   creator = "me";
   lastModifiedBy = "him";
+//   created = new Date().toLocaleDateString("en-US", {                             //? One Method
+//       timeZone: "Asia/karachi"
+//   })
 
-  // ================Console logs================
-  // console.log(workbook)
-  console.log(created, creator, lastModifiedBy);
+created = `${new Date().toLocaleDateString()}  ${new Date().toLocaleTimeString()}`
+
+
+
+  //? ================Console logs================
+  //* console.log(workbook)
+//      console.log(created, creator, lastModifiedBy);
 }
